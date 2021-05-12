6570d8c85679360f2cfb6aa4fd7d1535af9163ec
patrickmklee <patrickmklee93@gmail.com>
2021-05-11 20:19:45 -0700

    Create project outline

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+// const routes = require('./routes');
+// const sequelize = require('./config/connection.js');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+// app.use(routes);
+
+sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
